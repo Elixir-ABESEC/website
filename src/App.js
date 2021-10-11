b0c054e4ed113e34216c184be5e802387c4c3a70
Heikrana <heikrana@pm.me>
2021-10-11 12:26:46 +0530

    Format files to match prettier config

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,12 +3,12 @@ import React, { Fragment } from "react";
 import "./Navbar.css";
 
 function Navbar() {
-	return (
-		/* Write you HTML components/tags here. */
-		<Fragment>
-			<div>Hello World</div>
-		</Fragment>
-	);
+  return (
+    /* Write you HTML components/tags here. */
+    <Fragment>
+      <div>Hello World</div>
+    </Fragment>
+  );
 }
 
 export default Navbar;
