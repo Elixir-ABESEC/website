a6e6c0c808dd1aa747452ed60a5b898a4c77a2a3
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2021-10-12 23:20:14 +0530

    Added Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
-import React, { Fragment } from "react";
-
+import React from "react";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     /* Write you HTML components/tags here. */
-    <Fragment>
-      <div>Hello World</div>
-    </Fragment>
+    <div className="flex navbar">
+      <div className="Nav__left">
+        <div className="logo flex">
+          <img src="https://codechefvit.com/assets/images/logos/ccwhite.png" alt="logo" />
+        </div>
+      </div>
+      <div class="Nav__Right">
+        <ul className="List flex">
+          <li>Home</li>
+          <li>Event</li>
+          <li>Webinars</li>
+          <li>Projects</li>
+          <li>Teams</li>
+          <li>Sponsors</li>
+          <li>Updates</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
