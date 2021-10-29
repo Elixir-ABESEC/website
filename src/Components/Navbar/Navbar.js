8dd7841f6e0028a7c59d79f49455c621a11efcb3
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2021-10-29 22:04:17 +0530

    Add Navbar (#13)
    * added navbar to the web site
    
    * Removed the Subscribe Button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,9 +5,21 @@ import "./Navbar.css";
 function Navbar() {
   return (
     /* Write you HTML components/tags here. */
-    <Fragment>
-      <div>Hello World</div>
-    </Fragment>
+      <div className='navbar'>
+      <div className='logo navbar__left'>Elixir</div>
+      <div className='navbar_right'>
+      <div className='navbar__right__box__left'>
+        <div className='link'>Home</div>
+        <div className='link'>Event</div>
+        <div className='link'>Blogs</div>
+        <div className='link'>About</div>
+        <div className='link'>Contact</div>
+      </div>
+       <div className='navbar__right__box__right'>
+        <input type="search" placeholder='search'/>
+       </div>
+      </div>
+      </div>
   );
 }
 
