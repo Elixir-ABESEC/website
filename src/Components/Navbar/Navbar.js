4c309673fe08338d69cf93b9360ffbdc345eee38
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2021-11-03 01:37:58 +0530

    Add Footer (#15)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,21 +5,21 @@ import "./Navbar.css";
 function Navbar() {
   return (
     /* Write you HTML components/tags here. */
-      <div className='navbar'>
-      <div className='logo navbar__left'>Elixir</div>
-      <div className='navbar_right'>
-      <div className='navbar__right__box__left'>
-        <div className='link'>Home</div>
-        <div className='link'>Event</div>
-        <div className='link'>Blogs</div>
-        <div className='link'>About</div>
-        <div className='link'>Contact</div>
+    <div className="Navbar">
+      <div className="logo navbar__left">Elixir</div>
+      <div className="navbar_right">
+        <div className="navbar__right__box__left">
+          <div className="link">Home</div>
+          <div className="link">Event</div>
+          <div className="link">Blogs</div>
+          <div className="link">About</div>
+          <div className="link">Contact</div>
+        </div>
+        <div className="navbar__right__box__right">
+          <input type="search" placeholder="search" />
+        </div>
       </div>
-       <div className='navbar__right__box__right'>
-        <input type="search" placeholder='search'/>
-       </div>
-      </div>
-      </div>
+    </div>
   );
 }
 
